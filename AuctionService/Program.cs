257d158932d55439008c4f2ddd6b651353d69741
Book <bookmountain@hotmail.com>
2025-03-31 10:55:07 -0400

    init: added auction service

---
+++ AuctionService/Program.cs
@@ -0,0 +1,42 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+        builder.Services.AddControllers();
+        builder.Services.AddDbContext<AuctionDbContext>(
+            opt => opt.UseNpgsql(
+                builder.Configuration.GetConnectionString("DefaultConnection")
+            )
+        );
+        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+
+        app.UseAuthorization();
+
+        Console.WriteLine("Starting AuctionService...");
+
+        app.MapControllers();
+
+        try
+        {
+            DbInitializer.InitDb(app);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+
+        app.Run();
+    }
+}
